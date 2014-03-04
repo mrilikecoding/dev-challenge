fc5a36e41162565cfcf98adabc12a3bf76f9767c
mrilikecoding <nathanrgreen@gmail.com>
2014-03-04 14:54:14 -0600

    indentation weirdness

--- lib/challenge/api.rb
+++ lib/challenge/api.rb
@@ -1,19 +1,19 @@
 # API for getting records sorted by field and posting lines
 
 module Challenge
-	class API < Grape::API
-		format :json
+  class API < Grape::API
+    format :json
 
-		post 'records/:line' do
-			line = params[:line]
+    post 'records/:line' do
+      line = params[:line]
       puts line
-			Database.add_line(line)
-		end
+      Database.add_line(line)
+    end
 
-		get 'records/:primary_field' do
-			primary_field = params[:primary_field]
-			Challenge.sort_people(primary_field, params[:sort_by])
-		end
-	end
+    get 'records/:primary_field' do
+      primary_field = params[:primary_field]
+      Challenge.sort_people(primary_field, params[:sort_by])
+    end
+  end
 end
 
